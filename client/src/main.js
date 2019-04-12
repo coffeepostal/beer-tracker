6bea796dc6f97783160e45095f4280cd273b5db7
Adam Farnsworth <hello@farns.us>
2019-04-11 18:32:06 -0700

    Importing Vuetify package

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 Vue.config.productionTip = false
+
+//  Use Vuetify
+Vue.use(Vuetify)
 
 new Vue({
   router,
